89b4921394b5378efd392ec4d772b0b1ed843215
XaloEpshi <gonzalo.mellao.m@gmail.com>
2024-05-21 16:58:44 -0400

    Cambios en los estilos, eliminacion de los que no se estan necesitando

--- libreria-apilados/src/components/Header/Header.jsx
+++ libreria-apilados/src/components/Header/Header.jsx
@@ -5,27 +5,31 @@ import "./Header.css";
 
 const Header = () => {
   return (
-    <div className="holder">
-      <header className="header">
+    <div>
+      <header>
         <Navbar />
         <div className="header-content flex flex-c text-center text-white">
-          <h2 className="header-title text-capitalize">
-            Buscas un libro? Tenemos miles.
-          </h2>
           <br />
-          <p className="header-text fs-18 fw-3">
-            Descubre el rincón perfecto para los amantes de la lectura! 
-            </p>
-          <p className="header-text fs-18 fw-3">
+          <br />
+          <br />
+          <h2>Buscas un libro? Tenemos miles.</h2>
+          <br />
+          <p>Descubre el rincón perfecto para los amantes de la lectura!</p>
+          <p>
             En Librería Apilados, no solo encontrarás una vasta selección de
             títulos, sino también un espacio donde cada historia cobra vida.
+          </p>
+          <p>
             Desde aventuras épicas hasta los misterios más intrigantes, nuestra
             colección está cuidadosamente curada para inspirar, educar y
-            entretener. ¿Listo para sumergirte en tu próxima gran lectura?
-            Visítanos y déjate llevar por el encanto de las páginas que esperan
-            ser descubiertas.
+            entretener.
           </p>
-          <SearchForm /> 
+          <p>
+            ¿Listo para sumergirte en tu próxima gran lectura? Visítanos y
+            déjate llevar por el encanto de las páginas que esperan ser
+            descubiertas.
+          </p>
+          <SearchForm />
         </div>
       </header>
     </div>
